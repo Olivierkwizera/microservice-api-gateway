d9c195f36293d0b39faad6c4da20fee16a4d877e
Olivier KWIZERA <olivier.kwizera@emeal.nttdata.com>
2024-01-28 18:23:16 +0100

    Initial commit

---
+++ src/main/java/com/microservice/cloud/gateway/FallbackController.java
@@ -0,0 +1,20 @@
+package com.microservice.cloud.gateway;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class FallbackController {
+
+    @RequestMapping("/orderFallback")
+    public Mono<String> orderServiceFallback() {
+        return Mono.just("Order service is taking too long to respond or is down. Please try later");
+    }
+
+    @RequestMapping("/paymentFallback")
+    public Mono<String> paymentServiceFallback() {
+        return Mono.just("Payment is taking too long to respond or is down. Please try later");
+    }
+}
